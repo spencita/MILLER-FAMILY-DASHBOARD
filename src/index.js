e8cf88ff872652e6d73ca604dfc7e0f508c650de
spencita <shan.s.spence@gmail.com>
2026-05-04 12:07:17 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MillerFamilyDashboard from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+<MillerFamilyDashboard />
+</React.StrictMode>
+);
