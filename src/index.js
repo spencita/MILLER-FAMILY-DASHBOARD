9f7abd4d6a824365fbf948dbd2ba12be6e4768f3
spencita <shan.s.spence@gmail.com>
2026-05-04 13:14:59 -0400

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MillerFamilyDashboard from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<React.StrictMode>
-<MillerFamilyDashboard />
-</React.StrictMode>
-);
